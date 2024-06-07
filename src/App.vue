8b366d634f5f8665198ce3fade9740365b75f601
54x1 <five4x1@gmail.com>
2024-06-08 01:42:25 +1000

    update working base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Quiz />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Components_Quiz.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Quiz
   }
-}
+};
 </script>
 
 <style>
