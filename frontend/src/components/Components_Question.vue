7e1de27927fd05c1def3394f7f421548ec7a833f
54x1 <five4x1@gmail.com>
2024-06-08 02:15:20 +1000

    update

---
+++ frontend/src/components/Components_Question.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card shadow-lg p-4">
+    <h2 class="text-xl font-semibold mb-4">{{ question.question }}</h2>
+    <div v-for="option in question.options" :key="option">
+      <button class="btn btn-outline w-full mb-2" @click="$emit('answered', option)">
+        {{ option }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: Object
+  }
+};
+</script>
